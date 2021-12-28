65a281807bca83c9bbe758efc0f6736224632e25
diegomerola <diego_merola@hotmail.com>
2021-12-28 13:08:02 -0300

    modificacion en tailwind.config.js para deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.jsx"],
-  //content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["index.html", "./src/**/*.jsx"],
   variants: {
     theme: {
       extend: {},
